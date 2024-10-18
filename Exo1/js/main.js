ac202c8e74acdeb161a95931911ba2a34102d158
Giacomo Landolfo <giacomo.6719@gmail.com>
2024-10-18 16:46:39 +0200

    Added Final ver Exo 1

---
+++ Exo1/js/main.js
@@ -0,0 +1,3 @@
+function functionInfos(){
+    alert("class : Square\nbackground : #8DAE87\ncolor : white\nheight : 200px\nwidth : 200px\nDisplay : Flex\nfont-family : Times New Roman\nfont-size : 2em")
+}
