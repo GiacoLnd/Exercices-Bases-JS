e58b9b7ff7d7c4f7aa4823c4df72cac417fab986
Giacomo Landolfo <giacomo.6719@gmail.com>
2024-10-21 22:06:53 +0200

    Working Version Inclu. classType and Toggle

---
+++ Exo4-bis/js/main.js
@@ -0,0 +1,45 @@
+const originalBgColor = document.body.style.backgroundColor;
+let activeIcon = null
+
+function toggleEffects(icon, bgColor) {
+    icon.classList.toggle('rounded')
+    icon.classList.toggle('shadow')
+    icon.classList.toggle('show-text')
+    
+    document.body.style.backgroundColor = bgColor;
+}
+
+function resetBackground(icon) {
+    icon.classList.remove('rounded', 'shadow', 'show-text');
+    document.body.style.backgroundColor = originalBgColor;
+    
+    
+}
+
+const icons = document.querySelectorAll('.icon')
+
+icons.forEach(icon => {
+    icon.addEventListener('click', () => {
+        let bgColor;
+
+        if(icon.id === 'facebook') {
+            bgColor = '#3b5998'
+        } else if(icon.id === 'twitter') {
+            bgColor = '#00acee'
+        } else if(icon.id === 'instagram') {
+            bgColor = '#cd486b'
+        }
+
+        if (activeIcon === icon) {
+            toggleEffects(icon, originalBgColor)
+            resetBackground();
+            activeIcon = null;
+        } else {
+            if (activeIcon) {
+                activeIcon.classList.remove('rounded', 'shadow', 'show-text');
+            }
+        }
+        toggleEffects(icon, bgColor)
+        activeIcon = icon;
+        })
+})
