10adf22d6555e7efce8b79e5f6a11f683e003e54
Giacomo Landolfo <giacomo.6719@gmail.com>
2024-10-22 09:12:39 +0200

    Final ver + Responsive

--- Exo4-bis/js/main.js
+++ Exo4-bis/js/main.js
@@ -1,43 +1,43 @@
 const originalBgColor = document.body.style.backgroundColor;
-let activeIcon = null
+let activeIcon = null;
 
 function toggleEffects(icon, bgColor) {
-    icon.classList.toggle('rounded')
-    icon.classList.toggle('shadow')
-    icon.classList.toggle('show-text')
+    icon.classList.toggle('rounded');
+    icon.classList.toggle('shadow');
+    icon.classList.toggle('show-text');
     
     document.body.style.backgroundColor = bgColor;
 }
 
 function resetBackground(icon) {
     if(activeIcon){
-    activeIcon.classList.remove('rounded', 'shadow', 'show-text')
+    activeIcon.classList.remove('rounded', 'shadow', 'show-text');
     }
     document.body.style.backgroundColor = originalBgColor;
     
 }
 
-const icons = document.querySelectorAll('.icon')
+const icons = document.querySelectorAll('.icon');
 
 icons.forEach(icon => {
     icon.addEventListener('click', () => {
         let bgColor;
 
         if(icon.id === 'facebook') {
-            bgColor = '#3b5998'
+            bgColor = '#3b5998';
         } else if(icon.id === 'twitter') {
-            bgColor = '#00acee'
+            bgColor = '#00acee';
         } else if(icon.id === 'instagram') {
-            bgColor = '#cd486b'
+            bgColor = '#cd486b';
         }
 
         if (activeIcon === icon) {
             resetBackground();
             activeIcon = null;
         } else {
-        resetBackground();
-        toggleEffects(icon, bgColor)
-        activeIcon = icon;
+            resetBackground();
+            toggleEffects(icon, bgColor);
+            activeIcon = icon;
         }
     })
 })
